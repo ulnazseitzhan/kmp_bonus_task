cdf155a86a7c68a3d34880863898a8df51883a9d
ulnazseitzhan <ulnazzzseitzh@gmail.com>
2025-11-15 08:23:49 +0500

    Refactor Main.java to include KMP search tests
    Removed tips and added KMP search tests for various string lengths.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+
+        KMP kmp = new KMP();
+
+        System.out.println("\n TEST 1: Short String");
+        kmp.search("abcabc", "abc");
+
+        System.out.println("\n TEST 2: Medium String ");
+        kmp.search("aaabaaabaaabaaa", "aab");
+
+        System.out.println("\n TEST 3: Long String");
+        // Large input to demonstrate linear performance
+        String longText = "a".repeat(100000) + "b";
+        kmp.search(longText, "ab");
     }
 }
